7304c62547c6d822b1e3789b3b0c90f37db718e6
Jorge Garro <jorge.garroelizondo@jpschools.org>
2025-03-20 00:39:37 +0000

    range.js done

--- day-3/homework/range.js
+++ day-3/homework/range.js
@@ -23,10 +23,17 @@
  */
 function range(start, end) {
     // YOUR CODE GOES BELOW HERE //
-    
-    
-    
-    
+    var output = [];
+    if (start < end) {
+        for(var i = start; i <= end; i++) {
+            output.push(i);
+        }
+    } else {
+        for(var i = start; i >= end; i--) {
+            output.push(i);
+        }
+    }
+    return output;
     // YOUR CODE GOES ABOVE HERE //
 }
 
